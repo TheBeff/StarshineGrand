3abf72224c089fabe3027c0152fee91ffbdb7b15
Unknown <adh416@nyu.edu>
2018-01-27 16:45:03 -0500

    Star rotation script

---
+++ Assets/Scripts/StarRotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarRotation : MonoBehaviour {
+
+    public KeyCode rotateBody = KeyCode.Space;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(rotateBody))
+        {
+            transform.Rotate(0, +90, 0);
+        }
+	}
+}
