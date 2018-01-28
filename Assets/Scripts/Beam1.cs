2438a4ebee2b0419df03cd3f335b040779c54db0
Jeff Petriello <thebeff@gmail.com>
2018-01-28 13:45:21 -0500

    Beam switching SEEMINGLY works

--- Assets/Scripts/Beam1.cs
+++ Assets/Scripts/Beam1.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class Beam1 : MonoBehaviour {
 
-	GameManager gm;
+	private GameManager gm;
+	private int thisStarNum;
 
 	// Use this for initialization
 	void Start () {
 		gm = GameObject.Find ("Main Camera").GetComponent<GameManager> ();
+		thisStarNum = int.Parse(gameObject.tag.Substring (gameObject.tag.Length - 1, 1));
+		Debug.Log (thisStarNum);
+
 	}
 
 	void OnTriggerEnter(Collider star){
-		Debug.Log ("colliding with " + star);
-		if (star.CompareTag("Star2")) {
+		Debug.Log (gameObject + " colliding with " + star);
+		if (star.CompareTag("Star" + (thisStarNum+1))) {
 			Debug.Log("You hit the right star");
-			gm.activate("Star2");
-			gm.deactivate("Star1");
-		} else gm.restart();
+			gm.activate(2);
+			gm.deactivate(1);
+		} //else gm.restart();
 	}
 
 	// Update is called once per frame
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public List<GameObject> stars;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-	public void activate(string starTag){
-		Debug.Log ("activating " + starTag);
+	public void activate(int starNum){
+		Debug.Log ("activating star #" + starNum);
+		stars [starNum - 1].GetComponent<StarRotation> ().enabled = true;
+
 	}
 
-	public void deactivate(string starTag){
-		Debug.Log ("Deactivating " + starTag);
+	public void deactivate(int starNum){
+		Debug.Log ("Deactivating " + starNum);
+		stars [starNum - 1].GetComponent<StarRotation> ().enabled = false;
+
 	}
 
 	public void restart(){
