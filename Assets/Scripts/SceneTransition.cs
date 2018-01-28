0f152c64523e875fdee3e2e8ce81a5f4866494ce
francesca <francesca.carletto.leon@gmail.com>
2018-01-28 15:28:26 -0500

    created a start screen. make a star button that spins but can't be clicked because reasons

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+    public void NextScene()
+    {
+        Debug.Log("clicked");
+        SceneManager.LoadScene("Main Scene");
+    }
+}
+
+
